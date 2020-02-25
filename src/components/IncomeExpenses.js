1a7556f926c31eef0360ee5aae4896a66295d138
Khalal Walker <kwalker@metromile.com>
2020-02-24 22:32:20 -0800

    add income and balance logic

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export default function IncomeExpenses() {
+  const { transactions } = useContext(GlobalContext);
+  const amounts = transactions.map(({ amount }) => amount);
+  const addAmounts = (acc, item) => acc + item;
+  const income = amounts
+    .filter(amount => amount > 0)
+    .reduce(addAmounts, 0)
+    .toFixed(2);
+  const expense = amounts.filter(item => item < 0).reduce(addAmounts, 0);
+
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
         <p id="money-plus" className="money plus">
-          +$0.00
+          {`$${income}`}
         </p>
       </div>
       <div>
         <h4>Expense</h4>
         <p id="money-minus" className="money minus">
-          -$0.00
+          {`$${Math.abs(expense)}`}
         </p>
       </div>
     </div>
